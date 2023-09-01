f9f26273e0869c91234e83e80d4de7eea3c23ac8
Dan Lee <danielhlee4@gmail.com>
2023-09-01 14:54:59 -0400

    add html

--- form-app/src/App.js
+++ form-app/src/App.js
@@ -1,6 +1,11 @@
+import Form from './form';
+
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <div>
+      <h1>Form App</h1>
+      <Form />
+    </div>
   );
 }
 
--- form-app/src/form.jsx
+++ form-app/src/form.jsx
@@ -1,10 +1,45 @@
 import {useState, useEffect} from "react";
 
 function Form() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [phoneType, setPhoneType] = useState('');
+    const [staff, setStaff] = useState('');
+    const [bio, setBio] = useState('');
+    const [checked, setChecked] = useState('');
+
     return (
         <>
             <form className='form'>
-                
+                <h2>Sign Up</h2>
+                <label for="name">Name:
+                    <input id="name" type="text" value={name}/>
+                </label>
+                <br/>
+                <label for="email">Email:
+                    <input id="email" type="text" value={email}/>
+                </label>
+                <br/>
+                <label for="phone">Phone:
+                    <input id="phone" type="text" value={phone}/>
+                </label>
+                <br/>
+                <label for="phoneType">Phone Type:
+                    <input id="phoneType" type="text" value={phoneType}/>
+                </label>
+                <br/>
+                    Student<input id="staff-student" type="radio" name="staff" value="student"/> 
+                    Instructor<input id="staff-instructor" type="radio" name="staff" value="instructor"/>
+                <br/>
+                <label for="bio">Bio:
+                    <input id="bio" type="textarea" value={bio}/>
+                </label>
+                <br/>
+                Receive Email Notifications:<input id="checked" type="checkbox" value="yes"/>
+
+                <br/>
+
             </form>
         </>
     )
