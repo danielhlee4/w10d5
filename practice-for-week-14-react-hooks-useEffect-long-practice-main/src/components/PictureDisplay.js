4d018a4098a1ee38f667d403a4a3e8ac49eee0ae
Dan Lee <danielhlee4@gmail.com>
2023-09-03 22:28:31 -0400

    add useEffect for size

--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/PictureDisplay.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/PictureDisplay.js
@@ -10,7 +10,7 @@ import feather8 from "../images/feather8.svg";
 import feather9 from "../images/feather9.svg";
 import featherA from "../images/featherA.svg";
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 const feathers = [
@@ -30,9 +30,26 @@ function PictureDisplay ({ size, featherCount, featherColors }) {
   // useEffect(() => {
   //   console.log('PictureDisplay', size, featherCount, featherColors);
   // }, [size, featherCount, featherColors]);
+  const [sizeClass, setSizeClass] = useState('');
 
   useEffect(() => {
     console.log('PictureDisplay size', size);
+    let cname = '';
+    switch (size) {
+      case 'm':
+        cname = 'medium';
+        break;
+      case 'l':
+        cname = 'large';
+        break;
+      case 'xl':
+        cname = 'xlarge';
+        break;
+      default:
+        cname = 'small';
+        break;
+    }
+    setSizeClass(cname);
   }, [size]);
 
   useEffect(() => {
@@ -51,7 +68,7 @@ function PictureDisplay ({ size, featherCount, featherColors }) {
   }
 
   return (
-    <div className={`image-area medium`}>
+    <div className={`image-area ${sizeClass}`}>
       {colors.map((c, i) =>
         <img  
           key={feathers[i]} 
